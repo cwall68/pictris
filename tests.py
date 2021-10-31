5dd7909c0de9f65554e147b93e525a6da07b2710
cwall <christoph.wall@me.com>
2021-11-01 00:23:27 +0100

    Punktezähler eingebaut

--- tests.py
+++ tests.py
@@ -24,7 +24,7 @@ class Controls(QtWidgets.QMainWindow):
         self.setWindowTitle('Pictris Controls')
 
         self.filter_loop = QPushButton("Play", self)
-        self.filter_loop.setGeometry(30, 30, 130, 20)
+        self.filter_loop.setGeometry(170, 330, 130, 20)
         self.filter_loop.setCheckable(True)
         self.filter_loop.setStyleSheet(
             ":checked{background: solid red}"
@@ -33,6 +33,9 @@ class Controls(QtWidgets.QMainWindow):
             ":!checked{border: 2px solid red}"
             ":!checked{font-size: 10px}"
         )
+
+        self.point_counter = QtWidgets.QLabel(self)
+        self.point_counter.setGeometry(QtCore.QRect(170, 40, 47, 13))
 
 screen_width, screen_height= pyautogui.size()
 print(f'Width: {screen_width} / Height: {screen_height}')
@@ -84,6 +87,7 @@ def start():
     global screen
     global screen_width
     global screen_heigh
+    global font
 
     RED = (255, 0, 0)
     GRAY = (150, 150, 150)
@@ -97,6 +101,14 @@ def start():
 
     screen = pygame.display.set_mode(size=(w_floor, h_floor))
     pygame.display.set_caption("PicTris")
+
+    # font = pygame.font.Font(pygame.font.get_default_font(), 36)
+    #
+    # number = 12
+    #
+    # # now print the text
+    # text_surface = pygame.font.Font.render(font, f'Hello world {number}', True, (55, 55, 55))
+    # screen.blit(text_surface, dest=(50, 50))
 
     if init == True:
         spielbild = find_pic()
@@ -174,8 +186,16 @@ def mouse_move():
     global fade_factor
     global partslist
     global GRAY
-    partslist = []
+    global counter
+    global fertig
+    global font
 
+    partslist = []
+    fertig = False
+
+    if init == True:
+        counter = 0
+        alphawert = 255
 
     RED = (255, 0, 0)
     #GRAY = (150, 150, 150)
@@ -184,7 +204,7 @@ def mouse_move():
     #pygame.init()
 
     running = True
-    complete = False
+    fertig = False
 
     image = resize(spielbild)
     width, height = image.size
@@ -208,28 +228,32 @@ def mouse_move():
 
     img = pic_parts(x,y)
     part = [img, (x,y)]
-    #partslist.append(part)
     rect = img.get_rect()
     rect.center = w_floor // 2, pic_pos_y // 2
     moving = False
     randlist = []
+    zugzahl = 0
     while running:
         screen = pygame.display.set_mode(size=(w_floor, h_floor))
         full_img.set_alpha(alphawert)
         screen.fill(GRAY)
         screen.blit(full_img, (pic_pos_x,pic_pos_y))
+        font = pygame.font.Font(pygame.font.get_default_font(), 36)
+
         for element in partslist:
             screen.blit(element[0], element[1])
 
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
                 if event.key == pygame.K_SPACE:
                     game_rounds = 0
+                    counter = 0
                     init = True
                     return
 
@@ -238,7 +262,9 @@ def mouse_move():
                     moving = True
 
             elif event.type == pygame.MOUSEBUTTONUP:
+                zugzahl += 1
                 #screen_rect = img.get_rect()
+                # print(f'Zugzahl: {zugzahl}')
                 print(f'X: {rect.left} Ziel-X = {(x + (w_floor - width) / 2)}')
                 print(f'Y: {rect.top}')
                 drop_x = rect.left
@@ -246,20 +272,36 @@ def mouse_move():
                 drop_y = rect.top
                 ziel_y = int(y + pic_pos_y)
                 if drop_x <= ziel_x + tolerance and drop_x >= ziel_x - tolerance and drop_y <= ziel_y + tolerance and drop_y >= ziel_y - tolerance:
+                    # zugzahl += 1
                     part = [img, (int(x+pic_pos_x), int(y + pic_pos_y))]
                     partslist.append(part)
                     randlist.append((x, y))
+                    counter += 1
+                    print(f'Zugzahl: {zugzahl} / Counter: {counter}')
+                    # font = pygame.font.Font(pygame.font.get_default_font(), 36)
+                    # text_surface = pygame.font.Font.render(font, f'Hello world {counter}', True, (55, 55, 55))
+                    # text_surface = pygame.font.Font.render(font, f'Hello world {counter}', True, (55, 55, 55))
+                    # screen.blit(text_surface, dest=(50, 50))
+                    # pygame.display.flip()
                     for i in range(0,30):
                         blit_grid(grid, (255,255,255))
                         pygame.display.flip()
+
                     if len(randlist) == (anz_h * anz_w):
-                        GRAY = (255,255,255)
-                        complete = True
+                        #controlsWindow.point_counter.setText(str(counter))
+                        #GRAY = (255,255,255)
+                        fertig = True
                         init = False
+                        text_surface = pygame.font.Font.render(font, f'Punkte: {counter}', True, (55, 55, 55))
+                        screen.blit(text_surface, dest=(50, 50))
+                        pygame.display.flip()
                         success(6)
                         return
+                else:
+                    counter -= 1
+                    #controlsWindow.point_counter.setText(str(counter))
 
-                if complete == False:
+                if fertig == False:
                     x = part_size * random. randint(0,anz_w-1)
                     y = part_size * random. randint(0,anz_h-1)
                     while (x,y) in randlist:
@@ -279,18 +321,25 @@ def mouse_move():
                 #screen.blit(full_image, ((w - width) / 2, h - height))
                 #screen.blit(img, rect)
                 alphawert -= fade
+                if alphawert <= 0:
+                    alphawert = 0
+                #controlsWindow.point_counter.setText(str(counter))
 
-            elif event.type == pygame.MOUSEMOTION and moving:
-                if complete == False:
+            elif event.type == pygame.MOUSEMOTION and moving == True:
+                if fertig == False:
                     rect.move_ip(event.rel)
 
 
         screen.blit(img, rect)
 
+        text_surface = pygame.font.Font.render(font, f'Punkte: {counter}', True, (55, 55, 55))
+        screen.blit(text_surface, dest=(50, 50))
+
         pygame.draw.rect(screen, RED, rect, 1)
-        #pygame.draw.line(screen, RED, (0,0), (100, 100), 6)
+        #controlsWindow.point_counter.setText(str(counter))
         blit_grid(grid, RED)
         pygame.display.update()
+
 
 def pic_parts(x,y):
 
@@ -344,10 +393,8 @@ def success(anz):
             pygame.display.flip()
 
 
-
+    #sucht nach Zufall ein Bild aus dem Spiuelbildverzeichnis
 def find_pic():
-    import datetime
-    from datetime import datetime, date
     import os
 
     global game_dir
@@ -389,6 +436,8 @@ if __name__ == '__main__':
     #mouse_move()
     controlsWindow.show()
     controlsWindow.filter_loop.clicked.connect(start_game)
+    #controlsWindow.point_counter.setText("Punkte")
+    #controlsWindow.point_counter.show()
     # init = True
     # while True:
     #     start()
